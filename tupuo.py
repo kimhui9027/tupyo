adbd8291cd47ba6e02b986e41dfe2bf30dad28b4
kimhui9027 <66947451+kimhui9027@users.noreply.github.com>
2020-08-10 10:40:12 +0900

    Update tupuo.py

--- tupuo.py
+++ tupuo.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord import Client
 
 client: Client = discord.Client()
@@ -13,12 +14,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith(".투표"):
-        vote = message.content[4:].split(".")
+    if message.content.startswith("GYCG 투표"):
+        vote = message.content[8:].split(".")
         await message.channel.send('투표 주제' + ' = ' + vote[0])
         for i in range(1, len(vote)):
             choose = await message.channel.send('```' + vote[i] + '```')
         await message.channel.send("(원하는 이모지를 추가하시오.)")
 
+    if message.content.startswith("gycg 투표"):
+        vote = message.content[8:].split(".")
+        await message.channel.send('투표 주제' + ' = ' + vote[0])
+        for i in range(1, len(vote)):
+            choose = await message.channel.send('```' + vote[i] + '```')
+        await message.channel.send("(원하는 이모지를 추가하시오.)")
 
-client.run("NzMwNjg1MzEzNDU3ODQ4MzYw.XwbFug.9PInyRyT3k3ErHEdUlngW3nBojc")
+    if message.content.startswith("/..공지.."):
+        channelid = "742189410501525514"
+        msg = message.content[8:]
+        await client.get_channel(int(channelid)).send(msg)
+
+access_token = os.environ("BOT_TOKEN")
+client.run(access_token)
